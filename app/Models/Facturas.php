865d934478e3875a37033e37a3d7a0745c823b36
anibalcastro <anibalcastro1515@gmail.com>
2023-09-22 23:30:18 -0600

    Modelo Facturas

---
+++ app/Models/Facturas.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Facturas extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'orden_id',
+        'empresa_id',
+        'subtotal',
+        'iva',
+        'monto',
+        'fecha',
+        'metodo_pago',
+        'saldo_restante',
+        'comentarios',
+        'estado',
+        'cajero'
+    ];
+
+    public function ordenPedido(){
+        return $this->hasMany(OrdenPedido::class,'order_id', 'id');
+    }
+}
