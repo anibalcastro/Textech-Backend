8d77053de5c7ddd159864461ecb0c02e1ce51a7d
anibalcastro <anibalcastro1515@gmail.com>
2023-09-22 23:29:46 -0600

    Recursos de abonos

---
+++ app/Http/Resources/V1/AbonosResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AbonosResource extends JsonResource{
+
+    public function toArray(Request $request): array
+    {
+        return[
+            'id' => $this->id,
+            'factura_id' => $this->factura_id,
+            'monto' => $this->monto,
+            'metodo_pago' => $this->metodo_pago,
+            'comentarios' => $this->comentarios,
+            'estado' => $this->estado,
+            'cajero' => $this->cajero
+        ];
+    }
+
+}
