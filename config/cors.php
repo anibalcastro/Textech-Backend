f93771d4182544bcdabc48d4157186fe8a113d4e
anibalcastro <anibalcastro1515@gmail.com>
2023-10-23 22:00:57 -0600

    Configuracion de CORS

--- config/cors.php
+++ config/cors.php
@@ -16,7 +16,7 @@ return [
     */
 
     'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
